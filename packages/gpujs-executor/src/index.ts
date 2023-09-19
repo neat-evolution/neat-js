5dd00aa6018e21be11aa71c02c6589cec01e3754
Grady Kuhnline <github@heygrady.net>
2023-09-19 12:32:46 -0700

    feat: gpujs-executor

---
+++ packages/gpujs-executor/src/index.ts
@@ -0,0 +1,2 @@
+export * from './createExecutor.js'
+export * from './toActivationKernel.js'
---
+++ packages/gpujs-executor/test/createExecutor.bench.ts
@@ -0,0 +1,29 @@
+import { createExecutor as createVanillaExecutor } from '@neat-js/executor'
+import { createPhenotype } from '@neat-js/neat'
+import { bench, describe, expect } from 'vitest'
+
+import { createExecutor } from '../src/index.js'
+
+import { data } from './fixtures/irisInputs.js'
+import { createOrganism } from './fixtures/organisms.js'
+
+const organism = createOrganism()
+const phenotype = createPhenotype(organism.genome)
+const executor = createExecutor(phenotype, data.length)
+const vanillaExecutor = createVanillaExecutor(phenotype, data.length)
+
+describe('createExecutor', () => {
+  bench('gpujs executor', async () => {
+    const outputs = await executor(data)
+    expect(outputs).toBeDefined()
+    expect(outputs).toHaveLength(data.length)
+    expect(outputs[0]).toHaveLength(3)
+  })
+
+  bench('vanilla executor', async () => {
+    const outputs = await vanillaExecutor(data)
+    expect(outputs).toBeDefined()
+    expect(outputs).toHaveLength(data.length)
+    expect(outputs[0]).toHaveLength(3)
+  })
+})
