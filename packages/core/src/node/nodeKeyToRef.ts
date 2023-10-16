2370356cf03c0b9719ce12a0a5bf8e2054972f50
Grady Kuhnline <github@heygrady.net>
2023-10-16 09:42:48 -0700

    fix: optimize nodeKeyToRef

--- packages/core/src/node/nodeKeyToRef.ts
+++ packages/core/src/node/nodeKeyToRef.ts
@@ -1,28 +1,30 @@
 import type { NodeRef, NodeRefTuple } from './NodeRef.js'
 import type { NodeKey } from './nodeRefToKey.js'
 import type { NodeType } from './NodeType.js'
-import { nodeTypeToNumber, nodeNumberToType } from './nodeTypeToNumber.js'
+import { nodeTypeToNumber } from './nodeTypeToNumber.js'
 
 /**
  * @param {NodeKey} key a node key in the form of `${type}[${id}]`
  * @returns {NodeRef} a node ref
  */
 export const nodeKeyToRef = (key: NodeKey): NodeRef => {
-  const [type, id] = key.split('[') as [type: string, id: string]
+  const bracketIndex = key.indexOf('[')
   return {
-    type: type as NodeType,
-    id: Number(id.slice(0, -1)),
+    type: key.substring(0, bracketIndex) as NodeType,
+    id: +key.substring(bracketIndex + 1, key.length - 1),
   }
 }
 
 export const nodeKeyToType = (key: NodeKey): NodeType => {
-  const [type] = key.split('[') as [type: string, id: string]
-  return type as NodeType
+  return key.substring(0, key.indexOf('[')) as NodeType
 }
 
 export const nodeKeyToRefTuple = (key: NodeKey): NodeRefTuple => {
-  const [type, id] = key.split('[') as [type: string, id: string]
-  return [type as NodeType, Number(id.slice(0, -1))]
+  const bracketIndex = key.indexOf('[')
+  return [
+    key.substring(0, bracketIndex) as NodeType,
+    +key.substring(bracketIndex + 1, key.length - 1),
+  ]
 }
 
 /**
@@ -33,16 +35,9 @@ export const nodeKeyToRefTuple = (key: NodeKey): NodeRefTuple => {
 export const nodeKeyToNumericTuple = (
   key: NodeKey
 ): [type: number, id: number] => {
-  const [type, id] = key.split('[') as [type: string, id: string]
-  return [nodeTypeToNumber(type as NodeType), Number(id.slice(0, -1))]
+  const bracketIndex = key.indexOf('[')
+  return [
+    nodeTypeToNumber(key.substring(0, bracketIndex) as NodeType),
+    +key.substring(bracketIndex + 1, key.length - 1),
+  ]
 }
-
-/**
- * Used in fromSharedBuffer
- * @param {[type: number, id: number]} param0 a tuple of node type and id
- * @returns {string} a node key in the form of `${type}[${id}]`
- */
-export const tupleToNodeKey = ([type, id]: [
-  type: number,
-  id: number
-]): NodeKey => `${nodeNumberToType(type)}[${id}]`
