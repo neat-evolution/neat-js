5cf7864128a49fd7ff12115c52290442a9019101
Grady Kuhnline <github@heygrady.net>
2023-10-16 09:42:48 -0700

    fix: optimize nodeKeyToRef

--- packages/core/src/node/nodeRefToKey.ts
+++ packages/core/src/node/nodeRefToKey.ts
@@ -1,4 +1,5 @@
 import type { NodeRef, NodeRefTuple } from './NodeRef.js'
+import { nodeNumberToType } from './nodeTypeToNumber.js'
 
 /** `${type}[${id}]` */
 export type NodeKey = string
@@ -10,6 +11,16 @@ export const nodeRefToKey = (nodeRef: NodeRef): string => {
 export const nodeTupleToKey = (nodeTuple: NodeRefTuple): string => {
   return nodeTuple[0] + '[' + nodeTuple[1] + ']'
 }
+
+/**
+ * Used in fromSharedBuffer
+ * @param {[type: number, id: number]} param0 a tuple of node type and id
+ * @returns {string} a node key in the form of `${type}[${id}]`
+ */
+export const numericTupleToNodeKey = ([type, id]: [
+  type: number,
+  id: number
+]): NodeKey => `${nodeNumberToType(type)}[${id}]`
 
 export const toNodeKey = (type: NodeRef['type'], id: NodeRef['id']): string => {
   return type + '[' + id + ']'
