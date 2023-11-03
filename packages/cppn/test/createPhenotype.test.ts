c0dd3b303cbfdb71cc2d10498dead6bee95ea532
Grady Kuhnline <github@heygrady.net>
2023-11-03 15:08:19 -0700

    fix: some fixture tests

---
+++ packages/cppn/test/createPhenotype.test.ts
@@ -0,0 +1,79 @@
+import { createSyncExecutor } from '@neat-js/executor'
+import { describe, expect, test } from 'vitest'
+
+import { createPhenotype } from '../src/index.js'
+
+import { testCases, type TestCase } from './fixtures/cppn_phenotype/index.js'
+
+const inputs = [
+  [983040, -458752, 983040, -327680],
+  [-851968, -196608, 851968, 983040],
+  [851968, 327680, 983040, -983040],
+  [-851968, -851968, -851968, 458752],
+  [-851968, -983040, -983040, -196608],
+  [983040, -720896, -851968, -589824],
+  [851968, 196608, -983040, -720896],
+  [-983040, 458752, -983040, 983040],
+  [983040, 458752, -983040, 720896],
+  [-983040, -458752, 983040, 851968],
+  [-851968, -458752, 983040, -851968],
+  [851968, -65536, 983040, 196608],
+  [-983040, 196608, 851968, -983040],
+  [983040, 720896, -983040, -589824],
+  [-851968, 720896, 851968, -327680],
+  [851968, 720896, 983040, 327680],
+  [983040, 589824, 851968, -851968],
+  [-851968, -65536, -851968, 983040],
+  [-851968, 589824, -851968, 851968],
+  [-983040, 851968, -851968, 65536],
+  [851968, 589824, -983040, 327680],
+  [983040, -589824, -851968, 196608],
+  [-983040, -327680, -851968, -720896],
+  [-983040, -851968, -851968, -327680],
+  [-983040, 589824, 851968, 65536],
+  [851968, 458752, 983040, 65536],
+  [983040, -196608, 851968, -720896],
+  [-983040, 65536, 983040, 983040],
+  [851968, -589824, 983040, -65536],
+  [851968, 851968, 851968, -458752],
+  [-851968, 327680, -983040, -65536],
+  [-983040, -983040, 851968, -196608],
+]
+
+describe('createPhenotype', () => {
+  test.each([...testCases.entries()])(
+    'should export the same factoryOptions for test case #%d',
+    (_index, testCase: TestCase) => {
+      const { genome, factoryOptions } = testCase
+      const result = genome.toFactoryOptions()
+      expect(result).toEqual(factoryOptions)
+    }
+  )
+
+  test.each([...testCases.entries()])(
+    'should return the same outputs for test case #%d',
+    (_index, testCase: TestCase) => {
+      const { genome, phenotype } = testCase
+      const executorA = createSyncExecutor(createPhenotype(genome))
+      const executorB = createSyncExecutor(phenotype)
+      for (const input of inputs) {
+        const result = executorA(input) as [number, number]
+        const expected = executorB(input) as [number, number]
+        expect(result[0]).toBeCloseTo(expected[0], 5)
+        expect(result[1]).toBeCloseTo(expected[1], 5)
+      }
+    }
+  )
+
+  test.each([...testCases.entries()])(
+    'should create a phenotype for test case #%d',
+    (_index, testCase: TestCase) => {
+      const { genome, phenotype } = testCase
+      const result = createPhenotype(genome)
+      expect(result.length).toEqual(phenotype.length)
+      expect(result.inputs).toEqual(phenotype.inputs)
+      expect(result.outputs).toEqual(phenotype.outputs)
+      expect(result.actions.length).toEqual(phenotype.actions.length)
+    }
+  )
+})
---
+++ packages/es-hyperneat/test/exploreSubstrate.test.ts
@@ -0,0 +1,62 @@
+import { createPhenotype } from '@neat-js/cppn'
+import { type Point } from '@neat-js/hyperneat'
+import { describe, expect, test } from 'vitest'
+
+import { exploreSubstrate } from '../src/index.js'
+
+import {
+  testCases,
+  type TestCase,
+} from './fixtures/explore_substrates/index.js'
+
+const sortLayers = (layers: Point[][]): Point[][] => {
+  return layers.map((layer) => {
+    return layer.sort((a, b) => {
+      return a[0] - b[0] || a[1] - b[1]
+    })
+  })
+}
+
+describe('exploreSubstrate', () => {
+  test.each([...testCases.entries()])(
+    'should export the same factoryOptions for test case #%d',
+    (_index, testCase: TestCase) => {
+      const { genome, factoryOptions } = testCase
+      const result = genome.toFactoryOptions()
+      expect(result).toEqual(factoryOptions)
+    }
+  )
+
+  test.each([...testCases.entries()])(
+    'should create a phenotype for test case #%d',
+    (_index, testCase: TestCase) => {
+      const { genome, phenotype } = testCase
+      const result = createPhenotype(genome)
+      expect(result.length).toEqual(phenotype.length)
+      expect(result.inputs).toEqual(phenotype.inputs)
+      expect(result.outputs).toEqual(phenotype.outputs)
+      expect(result.actions.length).toEqual(phenotype.actions.length)
+    }
+  )
+
+  test.each([...testCases.entries()])(
+    'should return nodes for test case #%d',
+    (_index, testCase: TestCase) => {
+      const [nodes] = exploreSubstrate(...testCase.args)
+      expect(sortLayers(nodes)).toEqual(sortLayers(testCase.nodes))
+    }
+  )
+
+  test.each([...testCases.entries()])(
+    'should return connections for test case #%d',
+    (_index, testCase: TestCase) => {
+      const [, connections] = exploreSubstrate(...testCase.args)
+      for (const [i, connection] of connections.entries()) {
+        const expected = testCase.connections[i] as [string, string, number]
+        expect(connection[0]).toEqual(expected[0])
+        expect(connection[1]).toEqual(expected[1])
+        expect(connection[2]).toBeCloseTo(expected[2], 10)
+      }
+    }
+  )
+})
