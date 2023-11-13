85c633bdf88f534d239be7ca8cd1d68d1b08b84a
Grady Kuhnline <github@heygrady.net>
2023-11-12 16:19:03 -0800

    fix(hyperneat): remove reproducer options

--- packages/hyperneat/src/hyperneat.ts
+++ packages/hyperneat/src/hyperneat.ts
@@ -20,7 +20,6 @@ import type {
   EvolutionOptions,
   PopulationOptions,
   ReproducerFactory,
-  ReproducerFactoryOptions,
 } from '@neat-evolution/evolution'
 import { evolve, Population } from '@neat-evolution/evolution'
 import type {
@@ -33,47 +32,42 @@ import type {
 import { HyperNEATAlgorithm } from './HyperNEATAlgorithm.js'
 import type { HyperNEATGenomeOptions } from './HyperNEATGenomeOptions.js'
 
-export type HyperNEATPopulation<RFO extends ReproducerFactoryOptions> =
-  Population<
-    ConfigFactoryOptions,
-    null,
-    null,
-    ConfigData,
-    NEATConfig,
-    null,
-    null,
-    null,
-    null,
-    StateData,
-    NEATState,
-    CPPNNodeData,
-    NEATLinkData,
-    CPPNGenomeFactoryOptions,
-    HyperNEATGenomeOptions,
-    CPPNGenomeData<HyperNEATGenomeOptions>,
-    CPPNNodeFactoryOptions,
-    CPPNNode,
-    LinkFactoryOptions,
-    NEATLink,
-    CPPNGenome<HyperNEATGenomeOptions>,
-    typeof HyperNEATAlgorithm,
-    RFO
-  >
+export type HyperNEATPopulation = Population<
+  ConfigFactoryOptions,
+  null,
+  null,
+  ConfigData,
+  NEATConfig,
+  null,
+  null,
+  null,
+  null,
+  StateData,
+  NEATState,
+  CPPNNodeData,
+  NEATLinkData,
+  CPPNGenomeFactoryOptions,
+  HyperNEATGenomeOptions,
+  CPPNGenomeData<HyperNEATGenomeOptions>,
+  CPPNNodeFactoryOptions,
+  CPPNNode,
+  LinkFactoryOptions,
+  NEATLink,
+  CPPNGenome<HyperNEATGenomeOptions>,
+  typeof HyperNEATAlgorithm
+>
 
-export type HyperNEATReproducerFactory<RFO extends ReproducerFactoryOptions> =
-  ReproducerFactory<
-    CPPNGenome<HyperNEATGenomeOptions>,
-    HyperNEATPopulation<RFO>,
-    RFO
-  >
+export type HyperNEATReproducerFactory = ReproducerFactory<
+  CPPNGenome<HyperNEATGenomeOptions>,
+  HyperNEATPopulation
+>
 
-export const hyperneat = async <RFO extends ReproducerFactoryOptions>(
-  createReproducer: HyperNEATReproducerFactory<RFO>,
-  evaluator: Evaluator,
+export const hyperneat = async (
+  createReproducer: HyperNEATReproducerFactory,
+  evaluator: Evaluator<any, any, any>,
   evolutionOptions: EvolutionOptions,
   neatConfigOptions: NEATConfigOptions,
   populationOptions: PopulationOptions,
-  reproducerOptions: RFO,
   genomeOptions: HyperNEATGenomeOptions
 ) => {
   const configProvider = HyperNEATAlgorithm.createConfig({
@@ -89,13 +83,12 @@ export const hyperneat = async <RFO extends ReproducerFactoryOptions>(
     outputs: 2,
   }
 
-  const population: HyperNEATPopulation<RFO> = new Population(
+  const population: HyperNEATPopulation = new Population(
     createReproducer,
     evaluator,
     HyperNEATAlgorithm,
     configProvider,
     populationOptions,
-    reproducerOptions,
     genomeOptions,
     cppnInitConfig
   )
