6a9b14b6f9f6ae9fad48abf15c62b6e44f236e0e
Grady Kuhnline <github@heygrady.net>
2023-11-12 16:14:29 -0800

    feat(environment): new standard environment; improve environment generics

--- packages/environment/src/EnvironmentFactory.ts
+++ packages/environment/src/EnvironmentFactory.ts
@@ -1,5 +1,17 @@
+import type { Executor, SyncExecutor } from '@neat-evolution/executor'
+
 import type { Environment } from './Environment.js'
 
-export type EnvironmentFactory = (
-  environmentData: SharedArrayBuffer | null
-) => Environment
+export type EnvironmentFactory<
+  EFO,
+  E extends SyncExecutor[],
+  EA extends Executor[],
+  ER
+> = (options: EFO) => Environment<EFO, E, EA, ER>
+
+export type StandardEnvironmentFactory<EFO> = EnvironmentFactory<
+  EFO,
+  [executor: SyncExecutor],
+  [executor: Executor],
+  number
+>
