43b9935117c0510f7ad8c8bbf9dbaa46ca2f02ed
Grady Kuhnline <github@heygrady.net>
2023-11-29 08:59:31 -0800

    feat(hyperneat): create population

--- packages/hyperneat/src/hyperneat.ts
+++ packages/hyperneat/src/hyperneat.ts
@@ -1,66 +1,16 @@
-import type {
-  ConfigData,
-  ConfigFactoryOptions,
-  InitConfig,
-  LinkFactoryOptions,
-  NEATConfigOptions,
-  StateData,
-} from '@neat-evolution/core'
-import { defaultNEATConfigOptions } from '@neat-evolution/core'
-import {
-  type CPPNGenome,
-  type CPPNGenomeData,
-  type CPPNGenomeFactoryOptions,
-  type CPPNNodeData,
-  type CPPNNode,
-  type CPPNNodeFactoryOptions,
-} from '@neat-evolution/cppn'
+import type { NEATConfigOptions } from '@neat-evolution/core'
 import type { Evaluator } from '@neat-evolution/evaluator'
 import type {
   EvolutionOptions,
   PopulationOptions,
-  ReproducerFactory,
 } from '@neat-evolution/evolution'
-import { evolve, Population } from '@neat-evolution/evolution'
-import type {
-  NEATConfig,
-  NEATLink,
-  NEATLinkData,
-  NEATState,
-} from '@neat-evolution/neat'
+import { evolve } from '@neat-evolution/evolution'
 
-import { HyperNEATAlgorithm } from './HyperNEATAlgorithm.js'
+import {
+  createPopulation,
+  type HyperNEATReproducerFactory,
+} from './createPopulation.js'
 import type { HyperNEATGenomeOptions } from './HyperNEATGenomeOptions.js'
-
-export type HyperNEATPopulation = Population<
-  ConfigFactoryOptions,
-  null,
-  null,
-  ConfigData,
-  NEATConfig,
-  null,
-  null,
-  null,
-  null,
-  StateData,
-  NEATState,
-  CPPNNodeData,
-  NEATLinkData,
-  CPPNGenomeFactoryOptions,
-  HyperNEATGenomeOptions,
-  CPPNGenomeData<HyperNEATGenomeOptions>,
-  CPPNNodeFactoryOptions,
-  CPPNNode,
-  LinkFactoryOptions,
-  NEATLink,
-  CPPNGenome<HyperNEATGenomeOptions>,
-  typeof HyperNEATAlgorithm
->
-
-export type HyperNEATReproducerFactory = ReproducerFactory<
-  CPPNGenome<HyperNEATGenomeOptions>,
-  HyperNEATPopulation
->
 
 export const hyperneat = async (
   createReproducer: HyperNEATReproducerFactory,
@@ -70,27 +20,12 @@ export const hyperneat = async (
   populationOptions: PopulationOptions,
   genomeOptions: HyperNEATGenomeOptions
 ) => {
-  const configProvider = HyperNEATAlgorithm.createConfig({
-    neat: neatConfigOptions ?? defaultNEATConfigOptions,
-  })
-
-  // capture the real initConfig for createPhenotype later
-  genomeOptions.initConfig = evaluator.environment.description
-
-  // force initConfig to be 4, 2
-  const cppnInitConfig: InitConfig = {
-    inputs: 4,
-    outputs: 2,
-  }
-
-  const population: HyperNEATPopulation = new Population(
+  const population = createPopulation(
     createReproducer,
     evaluator,
-    HyperNEATAlgorithm,
-    configProvider,
+    neatConfigOptions,
     populationOptions,
-    genomeOptions,
-    cppnInitConfig
+    genomeOptions
   )
 
   await evolve(population, evolutionOptions)
