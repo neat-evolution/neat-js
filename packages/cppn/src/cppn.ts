f678def2b468a941260a22deb0aac8b7bedc61e9
Grady Kuhnline <github@heygrady.net>
2023-11-29 09:00:00 -0800

    feat(cppn): create population

--- packages/cppn/src/cppn.ts
+++ packages/cppn/src/cppn.ts
@@ -1,65 +1,16 @@
-import type {
-  ConfigData,
-  ConfigFactoryOptions,
-  LinkFactoryOptions,
-  NEATConfigOptions,
-  StateData,
-} from '@neat-evolution/core'
-import { defaultNEATConfigOptions } from '@neat-evolution/core'
+import type { NEATConfigOptions } from '@neat-evolution/core'
 import type { Evaluator } from '@neat-evolution/evaluator'
 import type {
   EvolutionOptions,
   PopulationOptions,
-  ReproducerFactory,
 } from '@neat-evolution/evolution'
-import { evolve, Population } from '@neat-evolution/evolution'
-import type {
-  NEATConfig,
-  NEATLink,
-  NEATLinkData,
-  NEATState,
-} from '@neat-evolution/neat'
+import { evolve } from '@neat-evolution/evolution'
 
-import { CPPNAlgorithm } from './CPPNAlgorithm.js'
-import type { CPPNGenome } from './CPPNGenome.js'
-import type { CPPNGenomeData } from './CPPNGenomeData.js'
-import type {
-  CPPNGenomeFactoryOptions,
-  CPPNNodeData,
-} from './CPPNGenomeFactoryOptions.js'
-import { type CPPNGenomeOptions } from './CPPNGenomeOptions.js'
-import type { CPPNNode } from './CPPNNode.js'
-import type { CPPNNodeFactoryOptions } from './CPPNNodeFactoryOptions.js'
-
-export type CPPNPopulation = Population<
-  ConfigFactoryOptions,
-  null,
-  null,
-  ConfigData,
-  NEATConfig,
-  null,
-  null,
-  null,
-  null,
-  StateData,
-  NEATState,
-  CPPNNodeData,
-  NEATLinkData,
-  CPPNGenomeFactoryOptions,
-  CPPNGenomeOptions,
-  CPPNGenomeData<CPPNGenomeOptions>,
-  CPPNNodeFactoryOptions,
-  CPPNNode,
-  LinkFactoryOptions,
-  NEATLink,
-  CPPNGenome<CPPNGenomeOptions>,
-  typeof CPPNAlgorithm
->
-
-export type CPPNReproducerFactory = ReproducerFactory<
-  CPPNGenome<CPPNGenomeOptions>,
-  CPPNPopulation
->
+import type { CPPNGenomeOptions } from './CPPNGenomeOptions.js'
+import {
+  createPopulation,
+  type CPPNReproducerFactory,
+} from './createPopulation.js'
 
 export const cppn = async (
   createReproducer: CPPNReproducerFactory,
@@ -69,20 +20,12 @@ export const cppn = async (
   populationOptions: PopulationOptions,
   genomeOptions: CPPNGenomeOptions
 ) => {
-  const configProvider = CPPNAlgorithm.createConfig({
-    neat: neatConfigOptions ?? defaultNEATConfigOptions,
-  })
-
-  const initConfig = evaluator.environment.description
-
-  const population: CPPNPopulation = new Population(
+  const population = createPopulation(
     createReproducer,
     evaluator,
-    CPPNAlgorithm,
-    configProvider,
+    neatConfigOptions,
     populationOptions,
-    genomeOptions,
-    initConfig
+    genomeOptions
   )
 
   await evolve(population, evolutionOptions)
---
+++ packages/cppn/src/createPopulation.ts
@@ -0,0 +1,87 @@
+import type {
+  ConfigData,
+  ConfigFactoryOptions,
+  LinkFactoryOptions,
+  NEATConfigOptions,
+  StateData,
+} from '@neat-evolution/core'
+import { defaultNEATConfigOptions } from '@neat-evolution/core'
+import type { Evaluator } from '@neat-evolution/evaluator'
+import type {
+  PopulationOptions,
+  ReproducerFactory,
+} from '@neat-evolution/evolution'
+import { Population } from '@neat-evolution/evolution'
+import type {
+  NEATConfig,
+  NEATLink,
+  NEATLinkData,
+  NEATState,
+} from '@neat-evolution/neat'
+
+import { CPPNAlgorithm } from './CPPNAlgorithm.js'
+import type { CPPNGenome } from './CPPNGenome.js'
+import type { CPPNGenomeData } from './CPPNGenomeData.js'
+import type {
+  CPPNGenomeFactoryOptions,
+  CPPNNodeData,
+} from './CPPNGenomeFactoryOptions.js'
+import type { CPPNGenomeOptions } from './CPPNGenomeOptions.js'
+import type { CPPNNode } from './CPPNNode.js'
+import type { CPPNNodeFactoryOptions } from './CPPNNodeFactoryOptions.js'
+
+export type CPPNPopulation = Population<
+  ConfigFactoryOptions,
+  null,
+  null,
+  ConfigData,
+  NEATConfig,
+  null,
+  null,
+  null,
+  null,
+  StateData,
+  NEATState,
+  CPPNNodeData,
+  NEATLinkData,
+  CPPNGenomeFactoryOptions,
+  CPPNGenomeOptions,
+  CPPNGenomeData<CPPNGenomeOptions>,
+  CPPNNodeFactoryOptions,
+  CPPNNode,
+  LinkFactoryOptions,
+  NEATLink,
+  CPPNGenome<CPPNGenomeOptions>,
+  typeof CPPNAlgorithm
+>
+
+export type CPPNReproducerFactory = ReproducerFactory<
+  CPPNGenome<CPPNGenomeOptions>,
+  CPPNPopulation
+>
+
+export const createPopulation = (
+  createReproducer: CPPNReproducerFactory,
+  evaluator: Evaluator<any, any, any>,
+  neatConfigOptions: NEATConfigOptions,
+  populationOptions: PopulationOptions,
+  genomeOptions: CPPNGenomeOptions
+): CPPNPopulation => {
+  const configProvider = CPPNAlgorithm.createConfig({
+    neat: neatConfigOptions ?? defaultNEATConfigOptions,
+  })
+
+  const initConfig = evaluator.environment.description
+
+  const population: CPPNPopulation = new Population(
+    createReproducer,
+    evaluator,
+    CPPNAlgorithm,
+    configProvider,
+    populationOptions,
+    genomeOptions,
+    initConfig
+  )
+
+  return population
+}
