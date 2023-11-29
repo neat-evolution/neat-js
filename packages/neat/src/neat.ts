4988b6a85ef31770048f30b8197d9ca09819c38f
Grady Kuhnline <github@heygrady.net>
2023-11-29 08:59:17 -0800

    feat(neat): create population

--- packages/neat/src/neat.ts
+++ packages/neat/src/neat.ts
@@ -1,63 +1,16 @@
-import type {
-  ConfigData,
-  ConfigFactoryOptions,
-  LinkFactoryOptions,
-  NEATConfigOptions,
-  NodeFactoryOptions,
-  StateData,
-} from '@neat-evolution/core'
-import { defaultNEATConfigOptions } from '@neat-evolution/core'
+import type { NEATConfigOptions } from '@neat-evolution/core'
 import type { Evaluator } from '@neat-evolution/evaluator'
+import { evolve } from '@neat-evolution/evolution'
 import type {
   EvolutionOptions,
   PopulationOptions,
-  ReproducerFactory,
 } from '@neat-evolution/evolution'
-import { evolve, Population } from '@neat-evolution/evolution'
 
-import { NEATAlgorithm } from './NEATAlgorithm.js'
-import type { NEATConfig } from './NEATConfig.js'
-import type { NEATGenome } from './NEATGenome.js'
-import type { NEATGenomeData } from './NEATGenomeData.js'
-import type {
-  NEATGenomeFactoryOptions,
-  NEATHiddenNodeData,
-  NEATLinkData,
-} from './NEATGenomeFactoryOptions.js'
+import {
+  createPopulation,
+  type NEATReproducerFactory,
+} from './createPopulation.js'
 import type { NEATGenomeOptions } from './NEATGenomeOptions.js'
-import type { NEATLink } from './NEATLink.js'
-import type { NEATNode } from './NEATNode.js'
-import type { NEATState } from './NEATState.js'
-
-export type NEATPopulation = Population<
-  ConfigFactoryOptions,
-  null,
-  null,
-  ConfigData,
-  NEATConfig,
-  null,
-  null,
-  null,
-  null,
-  StateData,
-  NEATState,
-  NEATHiddenNodeData,
-  NEATLinkData,
-  NEATGenomeFactoryOptions,
-  NEATGenomeOptions,
-  NEATGenomeData,
-  NodeFactoryOptions,
-  NEATNode,
-  LinkFactoryOptions,
-  NEATLink,
-  NEATGenome,
-  typeof NEATAlgorithm
->
-
-export type NEATReproducerFactory = ReproducerFactory<
-  NEATGenome,
-  NEATPopulation
->
 
 export const neat = async (
   createReproducer: NEATReproducerFactory,
@@ -67,18 +20,12 @@ export const neat = async (
   populationOptions: PopulationOptions,
   genomeOptions: NEATGenomeOptions
 ) => {
-  const configProvider = NEATAlgorithm.createConfig({
-    neat: neatConfigOptions ?? defaultNEATConfigOptions,
-  })
-  const initConfig = evaluator.environment.description
-  const population: NEATPopulation = new Population(
+  const population = createPopulation(
     createReproducer,
     evaluator,
-    NEATAlgorithm,
-    configProvider,
+    neatConfigOptions,
     populationOptions,
-    genomeOptions,
-    initConfig
+    genomeOptions
   )
 
   await evolve(population, evolutionOptions)
