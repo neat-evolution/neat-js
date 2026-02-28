652e758a242c66f971c6c87ffc076efa1292e3d3
Grady Kuhnline <github@heygrady.net>
2026-02-27 17:18:28 -0800

    refactor(cppn): adopt AlgorithmContext-based generics
    - Add CPPNContext.ts defining the CPPN algorithm context
    - Replace verbose type param lists in CPPNAlgorithm, CPPNGenome, CPPNNode
    - Update createGenome, createNode, createPopulation to use CPPNContext
    - Export CPPNContext from package index

---
+++ packages/cppn/src/CPPNContext.ts
@@ -0,0 +1,53 @@
+import type {
+  AlgorithmContext,
+  ConfigData,
+  ConfigFactoryOptions,
+  LinkFactoryOptions,
+  StateData,
+} from '@neat-evolution/core'
+import type { NEATConfig, NEATLink, NEATLinkData, NEATState } from '@neat-evolution/neat'
+
+import type { CPPNGenome } from './CPPNGenome.js'
+import type { CPPNGenomeData } from './CPPNGenomeData.js'
+import type {
+  CPPNGenomeFactoryOptions,
+  CPPNNodeData,
+} from './CPPNGenomeFactoryOptions.js'
+import type { CPPNGenomeOptions } from './CPPNGenomeOptions.js'
+import type { CPPNNode } from './CPPNNode.js'
+import type { CPPNNodeFactoryOptions } from './CPPNNodeFactoryOptions.js'
+
+export interface CPPNContext<GO extends CPPNGenomeOptions = CPPNGenomeOptions>
+  extends AlgorithmContext {
+  Config: {
+    FactoryOptions: ConfigFactoryOptions
+    NodeOptions: null
+    LinkOptions: null
+    Data: ConfigData
+    Type: NEATConfig
+  }
+  State: {
+    Data: StateData
+    NodeData: null
+    LinkData: null
+    Node: null
+    Link: null
+    Type: NEATState
+  }
+  Node: {
+    HiddenData: CPPNNodeData
+    FactoryOptions: CPPNNodeFactoryOptions
+    Type: CPPNNode
+  }
+  Link: {
+    Data: NEATLinkData
+    FactoryOptions: LinkFactoryOptions
+    Type: NEATLink
+  }
+  Genome: {
+    FactoryOptions: CPPNGenomeFactoryOptions
+    Options: GO
+    Data: CPPNGenomeData<GO>
+    Type: CPPNGenome<GO>
+  }
+}
--- packages/cppn/src/createNode.ts
+++ packages/cppn/src/createNode.ts
@@ -1,31 +1,29 @@
-import type { NodeFactory } from '@neat-evolution/core'
+import type { NodeFactory, NodeTypeOf } from '@neat-evolution/core'
 
+import type { CPPNContext } from './CPPNContext.js'
 import { CPPNNode } from './CPPNNode.js'
 import type { CPPNNodeFactoryOptions } from './CPPNNodeFactoryOptions.js'
+import type { CPPNGenomeOptions } from './CPPNGenomeOptions.js'
 import {
   type CPPNNodeOptions,
   defaultCPPNNodeOptions,
 } from './CPPNNodeOptions.js'
 
-export const createNodeFactory = (nodeOptions: CPPNNodeOptions) => {
-  const createNode: NodeFactory<
-    CPPNNodeFactoryOptions,
-    null,
-    null,
-    null,
-    CPPNNode
-  > = (factoryOptions: CPPNNodeFactoryOptions): CPPNNode => {
-    return new CPPNNode(factoryOptions, nodeOptions)
+export const createNodeFactory = <GO extends CPPNGenomeOptions>(
+  nodeOptions: CPPNNodeOptions
+) => {
+  const createNode: NodeFactory<CPPNContext<GO>> = (
+    factoryOptions: CPPNNodeFactoryOptions
+  ): NodeTypeOf<CPPNContext<GO>> => {
+    return new CPPNNode(factoryOptions, nodeOptions) as NodeTypeOf<
+      CPPNContext<GO>
+    >
   }
   return createNode
 }
 
-export const createNode: NodeFactory<
-  CPPNNodeFactoryOptions,
-  null,
-  null,
-  null,
-  CPPNNode
-> = (factoryOptions: CPPNNodeFactoryOptions): CPPNNode => {
+export const createNode: NodeFactory<CPPNContext> = (
+  factoryOptions: CPPNNodeFactoryOptions
+): CPPNNode => {
   return new CPPNNode(factoryOptions, defaultCPPNNodeOptions)
 }
