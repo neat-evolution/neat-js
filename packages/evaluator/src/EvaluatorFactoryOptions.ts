c5ff963f9186fd55265370f3c57eed4862e2b191
Grady Kuhnline <github@heygrady.net>
2026-02-27 17:18:30 -0800

    refactor(evaluator): adopt AlgorithmContext-based generics
    - Update EvaluatorFactoryOptions to use AlgorithmContext

--- packages/evaluator/src/EvaluatorFactoryOptions.ts
+++ packages/evaluator/src/EvaluatorFactoryOptions.ts
@@ -2,7 +2,7 @@ import type { EvaluationStrategy } from '@neat-evolution/evaluation-strategy'
 import type { ExecutorFactory } from '@neat-evolution/executor'
 import type { AnyGenome } from './types.js'
 
-export interface EvaluatorFactoryOptions<G extends AnyGenome<G> = any> {
+export interface EvaluatorFactoryOptions<G extends AnyGenome<any> = any> {
   createExecutor: ExecutorFactory
   strategy?: EvaluationStrategy<G>
 }
