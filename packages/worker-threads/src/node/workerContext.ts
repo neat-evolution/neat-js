8a5de2a938948da4ef2c124464a81e9ef65e1c48
Grady Kuhnline <github@heygrady.net>
2026-03-09 14:53:57 -0700

    refactor(worker-threads): adopt AlgorithmContext-based generics
    - Update MessageEvent, MessageListenerFn, Worker (browser and node)
    - Update workerContext for browser and node with refined types

--- packages/worker-threads/src/node/workerContext.ts
+++ packages/worker-threads/src/node/workerContext.ts
@@ -2,25 +2,21 @@ import { type MessagePort, parentPort } from 'node:worker_threads'
 
 import type { ContextEventTypes } from '../EventTypes.js'
 
+type ContextListener = (event: unknown) => void
+
 export const workerContext = {
   postMessage: (
-    message: any,
+    message: unknown,
     transferList?: Array<ArrayBuffer | MessagePort>
   ) => {
     parentPort?.postMessage(message, transferList)
   },
 
-  addEventListener: (
-    type: ContextEventTypes,
-    listener: (...args: any[]) => void
-  ) => {
+  addEventListener: (type: ContextEventTypes, listener: ContextListener) => {
     parentPort?.on(type, listener)
   },
 
-  removeEventListener: (
-    type: ContextEventTypes,
-    listener: (...args: any[]) => void
-  ) => {
+  removeEventListener: (type: ContextEventTypes, listener: ContextListener) => {
     parentPort?.off(type, listener)
   },
 
