bc4cce5c2342cefb578754e0f6cceecb276a48f3
Grady Kuhnline <github@heygrady.net>
2026-03-10 13:42:00 -0700

    feat(core): add AnyErasedAlgorithm and AnyErasedGenome type aliases
    - Add AnyErasedAlgorithm as Algorithm<any> for type-erased algorithm references
    - Add AnyErasedGenome as Genome<any> for type-erased genome references
    - Re-export both erased type aliases from the evaluator package

--- packages/evaluator/src/types.ts
+++ packages/evaluator/src/types.ts
@@ -1,1 +1,6 @@
-export type { AnyAlgorithm, AnyGenome } from '@neat-evolution/core'
+export type {
+  AnyAlgorithm,
+  AnyErasedAlgorithm,
+  AnyErasedGenome,
+  AnyGenome,
+} from '@neat-evolution/core'
