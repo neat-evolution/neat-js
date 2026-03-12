f2028ce0dc42196222118c6435b2bc4f0066faaa
Grady Kuhnline <github@heygrady.net>
2026-03-11 20:32:08 -0700

    feat(neat): implement PopulationCreator on NEATAlgorithm
    - NEATAlgorithm now satisfies PopulationCreator<NEATContext>
    - createPopulation builds config from configData (defaults to defaultNEATConfigOptions)
    - Sets usesCPPNActivations: false, enableCustomState: false

--- packages/neat/src/NEATAlgorithm.ts
+++ packages/neat/src/NEATAlgorithm.ts
@@ -1,4 +1,7 @@
 import type { Algorithm } from '@neat-evolution/core'
+import { defaultNEATConfigOptions } from '@neat-evolution/core'
+import type { PopulationCreator } from '@neat-evolution/evolution'
+import { Population } from '@neat-evolution/evolution'
 
 import { createConfig } from './createConfig.js'
 import { createGenome } from './createGenome.js'
@@ -8,13 +11,40 @@ import type { NEATContext } from './NEATContext.js'
 import { defaultNEATGenomeOptions } from './NEATGenomeOptions.js'
 import { writeBackWeights } from './writeBackWeights.js'
 
-export const NEATAlgorithm: Algorithm<NEATContext> = {
+export const NEATAlgorithm: Algorithm<NEATContext> &
+  PopulationCreator<NEATContext> = {
   name: 'NEAT',
   pathname: '@neat-evolution/neat',
   defaultOptions: defaultNEATGenomeOptions,
+  usesCPPNActivations: false,
+  enableCustomState: false,
   createConfig,
   createGenome,
   createPhenotype,
   createState,
   writeBackWeights,
+
+  createPopulation(
+    createReproducer,
+    evaluator,
+    configData,
+    populationOptions,
+    genomeOptions,
+    populationFactoryOptions
+  ) {
+    const configProvider = NEATAlgorithm.createConfig(
+      configData ?? { neat: defaultNEATConfigOptions }
+    )
+    const initConfig = evaluator.environment.description
+    return new Population<NEATContext>(
+      createReproducer,
+      evaluator,
+      NEATAlgorithm,
+      configProvider,
+      populationOptions,
+      genomeOptions,
+      initConfig,
+      populationFactoryOptions
+    )
+  },
 }
