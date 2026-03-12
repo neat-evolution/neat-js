83c4c5df0a03ecf5288897c80e06f9663bfe55f5
Grady Kuhnline <github@heygrady.net>
2026-03-11 20:32:08 -0700

    feat(cppn): implement PopulationCreator on CPPNAlgorithm
    - CPPNAlgorithm now satisfies PopulationCreator<CPPNContext>
    - createPopulation uses environment.description as initConfig directly
    - Sets usesCPPNActivations: true, enableCustomState: false

--- packages/cppn/src/CPPNAlgorithm.ts
+++ packages/cppn/src/CPPNAlgorithm.ts
@@ -1,4 +1,7 @@
 import type { Algorithm } from '@neat-evolution/core'
+import { defaultNEATConfigOptions } from '@neat-evolution/core'
+import type { PopulationCreator } from '@neat-evolution/evolution'
+import { Population } from '@neat-evolution/evolution'
 import {
   createConfig as createNEATConfig,
   createState as createNEATState,
@@ -10,13 +13,40 @@ import { createGenome } from './createGenome.js'
 import { createPhenotype } from './createPhenotype.js'
 import { writeBackWeights } from './writeBackWeights.js'
 
-export const CPPNAlgorithm: Algorithm<CPPNContext> = {
+export const CPPNAlgorithm: Algorithm<CPPNContext> &
+  PopulationCreator<CPPNContext> = {
   name: 'CPPN',
   pathname: '@neat-evolution/cppn',
   defaultOptions: defaultCPPNGenomeOptions,
+  usesCPPNActivations: true,
+  enableCustomState: false,
   createConfig: (factoryOptions) => createNEATConfig(factoryOptions),
   createGenome,
   createPhenotype,
   createState: () => createNEATState(),
   writeBackWeights,
+
+  createPopulation(
+    createReproducer,
+    evaluator,
+    configData,
+    populationOptions,
+    genomeOptions,
+    populationFactoryOptions
+  ) {
+    const configProvider = CPPNAlgorithm.createConfig(
+      configData ?? { neat: defaultNEATConfigOptions }
+    )
+    const initConfig = evaluator.environment.description
+    return new Population<CPPNContext>(
+      createReproducer,
+      evaluator,
+      CPPNAlgorithm,
+      configProvider,
+      populationOptions,
+      genomeOptions,
+      initConfig,
+      populationFactoryOptions
+    )
+  },
 }
